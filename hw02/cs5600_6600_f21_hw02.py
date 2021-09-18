57b8dcacdfe0ef79d3cd65edce9aa0cf894dc9ce
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-09-17 20:27:30 -0600

    initial completion

--- hw02/cs5600_6600_f21_hw02.py
+++ hw02/cs5600_6600_f21_hw02.py
@@ -8,13 +8,14 @@
 
 import numpy as np
 import pickle
+from itertools import accumulate
 from cs5600_6600_f21_hw02_data import *
 
 # sigmoid function and its derivative.
 # you'll use them in the training and fitting
 # functions below.
 def sigmoidf(x):
-    return 1 / (1 + exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 def sigmoidf_prime(x):
     #approximate
@@ -62,23 +63,43 @@ def build_121_nn():
 def build_1221_nn():
     return build_nn_wmats((1, 2, 2, 1))
 
+def feed_forward(X, nn):
+    return list(
+        accumulate(nn, lambda x, y: sigmoidf(x @ y), initial=X)
+    )
+
+def backprop(activations, y, X, nn):
+    yHat = activations[-1]
+    yHatErr = y - yHat
+    delta = yHatErr * sigmoidf_prime(yHat)
+
+    for i in range(1, len(nn)):
+        error = delta @ nn[-i].T
+        nn[-i] += activations[-i].T @ delta
+        delta = error * sigmoidf_prime(activations[-i - 1])
+
+    nn[0] += X.T @ delta
+
 ## Training 3-layer neural net.
 ## X is the matrix of inputs
 ## y is the matrix of ground truths.
 ## build is a nn builder function.
 def train_3_layer_nn(numIters, X, y, build):
-    ## your code here
-    pass
+    nn = build()
+    for _ in range(numIters):
+        activations = feed_forward(X, nn)
+        backprop(activations, y, X, nn)
+    return nn
 
 def train_4_layer_nn(numIters, X, y, build):
-    ## your code here
-    pass
+    #Same implementation
+    return train_3_layer_nn(numIters, X, y, build)
 
 def fit_3_layer_nn(x, wmats, thresh=0.4, thresh_flag=False):
-    ## your code here
-    pass
+    threshold = np.frompyfunc(lambda x: 1 if x > thresh else 0, 1, 1)
+    yHat = feed_forward(x, wmats)[-1]
+    return threshold(yHat) if thresh_flag else yHat
 
 def fit_4_layer_nn(x, wmats, thresh=0.4, thresh_flag=False):
-    ## your code here
-    pass
-
+    #Same implementation
+    return fit_3_layer_nn(x, wmats, thresh=thresh, thresh_flag=thresh_flag)
