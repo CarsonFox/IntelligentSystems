9f36353743e131361667d974008ae290f3a2b2f4
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-11-07 16:15:13 -0700

    Construct layers in order

--- project 1/image_anns.py
+++ project 1/image_anns.py
@@ -1,17 +1,17 @@
 #!/bin/python
 
+from os import path
+
 import numpy as np
 from tfl_image_anns import *
 
-if __name__ == "__main__":
-    epochs = 40
+def benchmark(name, layers):
+    epochs = 2
+    ann = make_image_ann(layers)
 
     test_accuracies = []
     valid_accuracies = []
-
-    for i in range(epochs//2):
-        layers = layers_16()
-        ann = make_image_ann(layers)
+    for _ in range(epochs // 2):
         train_tfl_image_ann_model(ann,
                                   BEE1_gray_train_X, BEE1_gray_train_Y,
                                   BEE1_gray_test_X, BEE1_gray_test_Y)
@@ -21,5 +21,21 @@ if __name__ == "__main__":
         valid_accuracies.append(
             validate_tfl_image_ann_model(ann, BEE1_gray_valid_X, BEE1_gray_valid_Y))
 
-    print(','.join(test_accuracies))
-    print(','.join(valid_accuracies))
+        print(test_accuracies, valid_accuracies)
+
+    save_path = path.join('./models/', name)
+    print(f'Saving ann to {save_path}')
+    ann.save(save_path)
+    return (name, test_accuracies, valid_accuracies)
+
+if __name__ == "__main__":
+    networks = {
+        '16': layers_16,
+        '32': layers_32,
+        '16x16': layers_16x16,
+        '16x16_dropout': layers_16x16_dropout,
+    }
+
+    benchmarks = [benchmark(name, layers()) for name, layers in networks.items()]
+
+    print(benchmarks)
