5235e747dee6295e0715e5048bc15dad3ba8bc70
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-11-08 18:03:18 -0700

    image ANN script

--- project 1/image_anns.py
+++ project 1/image_anns.py
@@ -3,23 +3,23 @@
 from os import path
 
 import numpy as np
-from tfl_image_convnets import *
+from tfl_image_anns import *
 
 def benchmark(name, layers):
-    epochs = 2
-    ann = make_convnet_model(layers)
+    epochs = 80
+    ann = make_image_ann(layers)
 
     test_accuracies = []
     valid_accuracies = []
     for _ in range(epochs // 2):
-        train_tfl_image_convnet_model(ann,
-                                  BEE1_train_X, BEE1_train_Y,
-                                  BEE1_test_X, BEE1_test_Y)
+        train_tfl_image_ann_model(ann,
+                                  BEE1_gray_train_X, BEE1_gray_train_Y,
+                                  BEE1_gray_test_X, BEE1_gray_test_Y)
 
         test_accuracies.append(
-            test_tfl_image_convnet_model(ann, BEE1_test_X, BEE1_test_Y))
+            test_tfl_image_ann_model(ann, BEE1_gray_test_X, BEE1_gray_test_Y))
         valid_accuracies.append(
-            validate_tfl_image_convnet_model(ann, BEE1_valid_X, BEE1_valid_Y))
+            validate_tfl_image_ann_model(ann, BEE1_gray_valid_X, BEE1_gray_valid_Y))
 
         print(test_accuracies, valid_accuracies)
 
@@ -31,7 +31,7 @@ def benchmark(name, layers):
 if __name__ == "__main__":
     with open('output', 'w') as f:
         networks = {
-            'layers_1conv_1fc': layers_1conv_1fc,
+            '64x64x64': layers_64x64x64,
         }
 
         benchmarks = [benchmark(name, layers()) for name, layers in networks.items()]
